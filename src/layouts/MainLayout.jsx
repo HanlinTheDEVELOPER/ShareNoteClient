00fd900a95a900b44b118797fd5f088a0d7b29e3
HanlinTheDEVELOPER <hanlinthedeveloper@gmail.com>
2024-01-30 21:30:07 +0630

    env fixed

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -6,12 +6,15 @@ import { useAuthStore } from "../store/authStore";
 export const StatusLoader = async () => {
   const [auth, setAuth] = useAuthStore((state) => [state.auth, state.setAuth]);
 
-  const res = await fetch("http://localhost:5173/api/v1/auth/status", {
-    credentials: "include",
-    headers: {
-      Authentication: auth?.token,
-    },
-  });
+  const res = await fetch(
+    `${import.meta.env.VITE_API_URL}/api/v1/auth/status`,
+    {
+      credentials: "include",
+      headers: {
+        Authentication: auth?.token,
+      },
+    }
+  );
   const status = await res.json();
   if (status.message === "Refresh Token Success") {
     setAuth(status.data);
