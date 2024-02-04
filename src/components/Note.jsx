a176d7b9bcaa0cc5e693fb86440be7509b885097
HanlinTheDEVELOPER <hanlinthedeveloper@gmail.com>
2024-02-05 00:17:24 +0630

    multiple user

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,8 +1,10 @@
 /* eslint-disable react/prop-types */
 
 import { Link } from "react-router-dom";
+import { useUserStore } from "../store/userStore";
 
 const Note = ({ note }) => {
+  const user = useUserStore((state) => state.user);
   return (
     <Link to={"/notes/1"}>
       <div className=" w-full border-t-4 border-t-teal-600 shadow-lg p-3">
@@ -16,9 +18,10 @@ const Note = ({ note }) => {
             <h3 className="text-xl font-medium">{note.title}</h3>
             <h5 className="text-xs text-gray-500">{note.sender.name}</h5>
           </div>
+          {note.sender._id === user._id && <p>Edit</p>}
         </div>
         <p className="text-sm text-gray-700 mt-1">
-          {note.content.substr(0, 100)} ...
+          {note.slug.substr(0, 100)} ...
         </p>
         <div className="mt-2 flex items-center justify-end gap-4"></div>
       </div>
