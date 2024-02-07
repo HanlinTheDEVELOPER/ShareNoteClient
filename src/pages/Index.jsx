fff5876906736bc6211b8d3657aa16675a7bc305
HanlinTheDEVELOPER <hanlinthedeveloper@gmail.com>
2024-02-07 22:36:19 +0630

    loading more icon

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -5,6 +5,7 @@ import { useInView } from "react-intersection-observer";
 
 import Note from "../components/Note";
 import { useNoteStore } from "../store/noteStore";
+import loading from "../assets/Bean Eater-1s-200px.gif";
 
 const Index = () => {
   const { ref, inView } = useInView();
@@ -40,15 +41,23 @@ const Index = () => {
   }, [inView, fetchNextPage, hasNextPage]);
 
   return (
-    <section className="grid grid-cols-1 sm:grid-cols-2  md:grid-cols-3 gap-6 mt-10 px-10">
-      {notes?.map((note, i) =>
-        notes.length === i + 1 ? (
-          <Note key={note._id} lastElRef={ref} note={note} />
-        ) : (
-          <Note key={note._id} note={note} />
-        )
-      )}
-    </section>
+    <>
+      <section className="grid grid-cols-1 sm:grid-cols-2  md:grid-cols-3 gap-6 mt-10 px-10">
+        {notes?.map((note, i) =>
+          notes.length === i + 1 ? (
+            <Note key={note._id} lastElRef={ref} note={note} />
+          ) : (
+            <Note key={note._id} note={note} />
+          )
+        )}
+      </section>
+      {status === "pending" ||
+        (isFetchingNextPage && (
+          <div className=" w-full text-xl opacity-80 flex justify-center font-bold mt-4 ">
+            <img alt="loading" src={loading} width={"50"} height={"50"} />
+          </div>
+        ))}
+    </>
   );
 };
 
