940835b6e96913b4d1495ce4767279b13f24f97f
Kaung Khant Maung <101945598+theoriginalcaleb@users.noreply.github.com>
2024-02-10 22:07:41 +0700

    vs-2

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -13,10 +13,6 @@ const Details = () => {
       </div>
       <div className="border-t-4 border-t-teal-600 shadow-lg p-3 mt-4">
         <h3 className="text-3xl font-medium">Lorem ipsum, dolor sit amet.</h3>
-        <p className="text-base mt-2">
-          lorem ipsum dolor sit amet consectetur adipisicing elit. Aut sapiente
-          necessitatibus molestiae
-        </p>
       </div>
     </section>
   );
