b36fdb367efabcbd82edbfb18e4439b13b0550bd
HanlinTheDEVELOPER <hanlinthedeveloper@gmail.com>
2024-03-06 22:41:39 +0630

    profile backgrouond

--- src/components/user/Profile.jsx
+++ src/components/user/Profile.jsx
@@ -7,10 +7,10 @@ const ProfileSection = () => {
 
   return (
     <Flex
-      bgColor={{ default: "red.600", dark: "red.800" }}
+      bgColor="rgb(70,150,42)"
       bgImage={{
         base: "linear-gradient(90deg, rgba(70,150,42,1) 0%, rgba(70,150,42,0.896796218487395) 45%, rgba(70,150,42,0.896796218487395) 100%)",
-        sm: "linear-gradient(90deg, rgba(233,235,238,1) 0%, rgba(70,150,42,0.7791491596638656) 30%, rgba(70,150,42,0.896796218487395) 45%, rgba(70,150,42,1) 100%)",
+        sm: "linear-gradient(90deg, rgba(70,150,42,0) 0%, rgba(164,185,157,0.11808473389355745) 5%, rgba(150,180,140,0.3169642857142857) 12%, rgba(146,178,135,0.4822303921568627) 18%, rgba(130,172,116,0.7903536414565826) 35%, rgba(108,164,89,0.9640231092436975) 45%, rgba(90,157,67,1) 71%, rgba(70,150,42,1) 100%, rgba(70,150,42,1) 100%)",
       }}
       height={{ base: "fit-content", sm: "300px" }}
       justifyContent="space-around"
