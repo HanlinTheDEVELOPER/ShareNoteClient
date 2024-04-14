ed669098097b4e59ad1b4f7d41e4a74107bda432
HanlinTheDEVELOPER <hanlinthedeveloper@gmail.com>
2024-04-14 16:56:33 +0630

    add another query key to  useSetUser

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
   const [setUserfn] = useSetUser();
   useEffect(() => {
     queryClient.prefetchQuery({
-      queryKey: ["user"],
+      queryKey: ["user", "profile"],
       queryFn: setUserfn,
     });
   }, [auth]);
